a8c1bbadef51858ebb130f70330d7b96f1b3f17b
Guillaume Normand <guillaumenormand@protonmail.com>
2023-06-28 22:09:23 -0500

    Guiguide style

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Full Width Pics v5.0.6 (https://startbootstrap.com/template/full-width-pics)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-full-width-pics/blob/master/LICENSE)
 */
 // This file is intentionally blank
